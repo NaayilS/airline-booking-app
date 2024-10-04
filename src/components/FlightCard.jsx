3112a7b0bc4e57c87574fa8e8de6030d6e3cc4da
NaayilS <sadiqnaayil@gmail.com>
2024-10-03 23:37:36 -0500

    Added function to make time appear easier to read on flight card

--- src/components/FlightCard.jsx
+++ src/components/FlightCard.jsx
@@ -1,5 +1,14 @@
 import React from 'react';
 
+// Function to format the date and time
+const formatDateTime = (isoString) => {
+  const date = new Date(isoString);
+  return new Intl.DateTimeFormat('en-US', {
+    dateStyle: 'medium',
+    timeStyle: 'short',
+  }).format(date);
+};
+
 const FlightCard = ({ flight }) => {
   const { itineraries, price } = flight;
   const firstSegment = itineraries[0].segments[0];
@@ -8,19 +17,20 @@ const FlightCard = ({ flight }) => {
   return (
     <div className="card">
       <div className="card-body">
-        <img 
+        <h5>
+          <img 
           src="https://mpng.hippopng.com/20180406/tie/kisspng-airplane-aircraft-maintenance-flight-transport-aeroplane-5ac7b14f05e3e0.1254718215230364950241.jpg" 
           alt="Airplane Icon" 
           className="airplane-icon" 
         />
-        <h5>{firstSegment.carrierCode} - Flight {firstSegment.number}</h5>
-        <p>From: {firstSegment.departure.iataCode} at {firstSegment.departure.at}</p>
-        <p>To: {lastSegment.arrival.iataCode} at {lastSegment.arrival.at}</p>
-        <p className="price">Price: {price.total} {price.currency}</p>
-        <button>Book Flight</button>
-      </div>
+        {firstSegment.carrierCode} - Flight {firstSegment.number}
+      </h5>
+      <p>From: {firstSegment.departure.iataCode} at {formatDateTime(firstSegment.departure.at)}</p>
+      <p>To: {lastSegment.arrival.iataCode} at {formatDateTime(lastSegment.arrival.at)}</p>
+      <p className="price">Price: {price.total} {price.currency}</p>
+      <button>Book Flight</button>
     </div>
-  );
+  </div>
+);
 };
-
 export default FlightCard;
